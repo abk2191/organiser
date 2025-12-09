ca2a4a2cc148414ffa702699ea7062e179c77713
Abhishek <abhishek.kabi.21@gmail.com>
2025-12-10 02:18:15 +0530

    Added new features

--- vite.config.js
+++ vite.config.js
@@ -19,7 +19,7 @@ export default defineConfig({
         theme_color: "#000000",
         icons: [
           {
-            src: "/android-icon-192x192.png",
+            src: "./android-icon-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
