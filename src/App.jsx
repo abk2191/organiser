04f83b480e6b7aced87b514c1b4c94484a214006
Abhishek <abhishek.kabi.21@gmail.com>
2025-12-11 20:28:40 +0530

    Added new features

--- src/App.jsx
+++ src/App.jsx
@@ -91,11 +91,11 @@ function App() {
               {" "}
               {lightTheme ? (
                 <>
-                  <i className="fa-solid fa-sun"></i> Light
+                  <i className="fa-solid fa-moon"></i> Dark
                 </>
               ) : (
                 <>
-                  <i className="fa-solid fa-moon"></i> Dark
+                  <i className="fa-solid fa-sun"></i> Light
                 </>
               )}{" "}
             </h2>
@@ -159,7 +159,7 @@ function App() {
               </div>
             </div>
 
-            <div className="theme-holder">
+            {/* <div className="theme-holder">
               <h2 className="theme-text">
                 {" "}
                 {lightTheme ? (
@@ -178,7 +178,7 @@ function App() {
                   <div class="handle"></div>
                 </label>
               </div>
-            </div>
+            </div> */}
 
             {/* <div className="brand-name">
               <p
